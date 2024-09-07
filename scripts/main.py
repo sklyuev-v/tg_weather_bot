c9df441ca4306c8bb8a146ea73a59d195615d1a8
Viktor Sklyuev <sklyevv@yandex.ru>
2024-09-07 20:20:57 +0300

    Add entry point and read token func

---
+++ scripts/main.py
@@ -0,0 +1,14 @@
+# Entry point for bot
+from typing import NoReturn
+
+
+def main() -> NoReturn:
+    """
+    Entry point for application
+    :return: NoReturn
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/read_bot_token.py
@@ -0,0 +1,4 @@
+# Read bot token from file
+
+def read_bot_token_from_file(filename: str) -> str:
+    pass
