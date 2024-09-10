2bbf49a2dc7d32e5ebe5fe852f359f1759a2c9fb
kdbvjDev <kadbvj@gmail.com>
2024-09-11 00:32:43 +0400

    add func

--- src/read_bot_token.py
+++
@@ -1,7 +0,0 @@
-# Read bot token from file
-
-def read_bot_token_from_file(filename: str) -> str:
-    with open(filename, "r") as file:
-        bot_token = file.read()
-        return bot_token
-
---
+++ src/weather.py
@@ -0,0 +1,47 @@
+from config import WEATHER_URL_ON_DAY
+from config import WEATHER_URL_ON_5_DAY
+from config import OPENWEATHER_API_KEY
+import requests
+import json
+
+
+def get_weather_on_day(city: str) -> str:
+    params: dict = {
+        'q': city,
+        'appid': OPENWEATHER_API_KEY,
+        'units': 'metric',
+        'lang': 'ru'
+    }
+    response = requests.get(url=WEATHER_URL_ON_DAY, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        city_name: str = data['name']
+        temp: float = data['main']['temp']
+        return f"Погода в {city_name}: {temp}"
+    else:
+        return "Не удалось получить данные о погоде. Проверьте правильность названия города."
+
+
+def get_weather_on_5_days(city: str):
+    params = {
+        'q': city,
+        'appid': OPENWEATHER_API_KEY,
+        'units': 'metric',
+        'lang': 'ru'
+    }
+    response = requests.get(url=WEATHER_URL_ON_5_DAY, params=params)
+
+    if response.status_code == 200:
+        data: dict = response.json()
+        data_list: list = data['list']
+        list_forecast = []
+
+        for el in data_list:
+            temp = el['main']['temp']
+            date = el['dt_txt']
+            city_name = el['city']['name']
+            list_forecast.append([temp, date, city_name])
+
+        return list_forecast
+    return None
