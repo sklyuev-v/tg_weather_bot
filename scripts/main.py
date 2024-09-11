314188b27781bba4dbd86a15288105d330cc1a0c
SemelaEV <semela.ev@gmail.com>
2024-09-11 19:02:49 +0300

    Add city_func

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,14 @@
 # Entry point for bot
 from typing import NoReturn
+import requests
+def input_city(message):
+    city = message.text
+    url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347'
+    weather_data = requests.get(url).json()
+    temperature = round(weather_data['main']['temp'])
+    answer = 'В городе ' + city + ' ' + str(temperature) + ' °C'
+
+
 
 
 def main() -> NoReturn:
